378b8188e78f9f16c602c55c1ff3bdd328b2a1ed
minjaekweon <mjkweon17@korea.ac.kr>
2024-08-15 20:27:34 +0900

    feat: 프론트 작업을 위한 example response 추가

--- src/main.py
+++ src/main.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "data": [
+            {
+                "id": "1",
+                "user_id": "1",
+                "user_name": "김현채",
+                "reviewContent": "23-1 컴과 전공이었던 권민재 교수님 개발학개론 전공도서였습니다. 아마 계속 이 교재로 하실 듯?",
+                "createdAt": "24.07.26"
+            },
+            {
+                "id": "1",
+                "user_id": "2",
+                "user_name": "현지수",
+                "reviewContent": "파이썬 처음 하시는 분들께 추천! 저는 이걸로 스터디 햇어용 우하하",
+                "createdAt": "24.07.26"
+            }
+        ]
+    }
